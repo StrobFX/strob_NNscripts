3bdf0c6944997f8e8e8e11f7319eefc146812f11
Jocelyn Strob Simard <12475879+StrobFX@users.noreply.github.com>
2019-05-25 23:11:31 -0400

    Update ping_iplistALL.sh

--- addNNip/ping_iplistALL.sh
+++ addNNip/ping_iplistALL.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `iplistALL.txt` ; do ping -c1 $i 2>&1 | tee >> ping-output.txt; done
+for i in `$HOME/komodo_script_strobFX/addNNip/iplistALL.txt` ; do ping -c1 $i 2>&1 | tee >> ping-output.txt; done
